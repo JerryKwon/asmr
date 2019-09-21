e137b29500bcf52b607f5ff63ae0c0c823694787
JerryKwon <kwonyoung234@gmail.com>
2019-09-22 00:31:52 +0900

    사진관리팝업입니다.(버튼색상조절, 리스너 설정)

--- src/asmr/PictureManage.java
+++ src/asmr/PictureManage.java
@@ -1,10 +1,18 @@
 package asmr;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -35,6 +43,13 @@ public class PictureManage extends JFrame{
 	private JTable eSelectedPictureList;
 	private JScrollPane selectedPictureListScroll;
 	
+	private Color blue = new Color(22,155,213);
+	private Color white = new Color(255,255,255);
+	
+	PictureManageButtonListener pictureManageButtonListener;
+	PictureManageFileMouseListener pictureManageFileMouseListener;
+	PictureManageSeletedFileMouseListener pictureManageSeletedFileMouseListener;
+	
 	GridBagLayout gridBagLayout;
 	GridBagConstraints gridBagConstraints;
 	
@@ -43,31 +58,55 @@ public class PictureManage extends JFrame{
 		gridBagLayout = new GridBagLayout();		
 		gridBagConstraints = new GridBagConstraints();
 		
+		pictureManageButtonListener = new PictureManageButtonListener();
+		pictureManageFileMouseListener = new PictureManageFileMouseListener();
+		pictureManageSeletedFileMouseListener = new PictureManageSeletedFileMouseListener();
+		
 		vPictureManage = new JLabel("사진관리");
 		
 		vPicturePath = new JLabel("사진경로");
 		xPicturePath = new JTextField(10);
 		xPicturePath.setEnabled(false);
 		pictureSearch = new JButton("찾아보기");
+		pictureSearch.setBackground(blue);
+		pictureSearch.setForeground(white);
+		pictureSearch.addActionListener(pictureManageButtonListener);
 		
 		ePictureList = new JTable(model1);
+		ePictureList.addMouseListener(pictureManageFileMouseListener);
 		pictureListScroll = new JScrollPane(ePictureList);
 		pictureListScroll.setPreferredSize(new Dimension(100,150));
 		
 		add = new JButton(">>");
+		add.addActionListener(pictureManageButtonListener);
+		
 		remove = new JButton("<<");
+		remove.addActionListener(pictureManageButtonListener);
 		
 		vSelectedPicture = new JLabel("선택된 사진");
 		
 		eSelectedPictureList = new JTable(model1);
+		eSelectedPictureList.addMouseListener(pictureManageSeletedFileMouseListener);
 		selectedPictureListScroll = new JScrollPane(eSelectedPictureList);
 		selectedPictureListScroll.setPreferredSize(new Dimension(100,150));
 		
 		vPreview = new JLabel("미리보기");
+		
+		File input = new File("images/dog_400_400.jpg");
+		BufferedImage image = ImageIO.read(input);
+		BufferedImage resized = resize(image,100,100);
 		imageLabel = new JLabel();
+		ImageIcon icon = new ImageIcon(resized);
+		imageLabel.setIcon(icon);
+		
 		
 		register = new JButton("등록");
+		register.setBackground(blue);
+		register.setForeground(white);
+		register.addActionListener(pictureManageButtonListener);
+		
 		cancel = new JButton("취소");
+		cancel.addActionListener(pictureManageButtonListener);
 		
 		PictureManageView();
 	}
@@ -84,28 +123,38 @@ public class PictureManage extends JFrame{
 		gridbagAdd(vPictureManage, 0, 0, 1, 1);
 		
 		gridbagAdd(vPicturePath, 0, 1, 1, 1);
-		gridbagAdd(xPicturePath, 1, 1, 1, 1);
-		gridbagAdd(pictureSearch, 2, 1, 1, 1);
+		
+		Component[] cops1 = {xPicturePath,pictureSearch};
+		CombinePanel picturePathPanel = new CombinePanel(cops1, 5, 0);
+		gridbagAdd(picturePathPanel, 1, 1, 2, 1);
 		
 		gridbagAdd(pictureListScroll, 1, 3, 1, 3);
 		
-		gridbagAdd(vSelectedPicture, 3, 2, 1, 1);
+		JPanel plainPanel = new JPanel();
+		plainPanel.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		plainPanel.add(vSelectedPicture);
+		plainPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		gridbagAdd(plainPanel, 3, 2, 1, 1);
 		
 		gridbagAdd(add, 2, 4, 1, 1);
 		gridbagAdd(remove, 2, 5, 1, 1);
 		
 		gridbagAdd(selectedPictureListScroll, 3, 3, 1, 3);
 		
-		gridbagAdd(vPreview, 4, 2, 1, 1);
-		imageLabel.setSize(new Dimension(200,200));
-		ImageIcon icon = new ImageIcon(ImageIO.read(new File("images/dog_100_100.jpg")));
-		imageLabel.setIcon(icon);
-		imageLabel.setSize(new Dimension(100,100));
+		JPanel plainPanel2 = new JPanel();
+		plainPanel2.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		plainPanel2.add(vPreview);
+		plainPanel2.setBorder(BorderFactory.createEmptyBorder(0, 45, 0, 0));
+		gridbagAdd(plainPanel2, 4, 2, 1, 1);
 		
-		gridbagAdd(imageLabel, 4, 3, 3, 3);
+		JPanel plainPanel3 = new JPanel();
+		plainPanel3.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		plainPanel3.add(imageLabel);
+		plainPanel3.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		gridbagAdd(plainPanel3, 4, 3, 3, 3);
 		
 		Component[] cops = {register,cancel};
-		CombinePanel buttonPanel = new CombinePanel(cops, true);
+		CombinePanel buttonPanel = new CombinePanel(cops,5,0);
 		buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 175, 0, 0));
 		gridbagAdd(buttonPanel, 0, 6, 7, 1);
 		
@@ -129,20 +178,78 @@ public class PictureManage extends JFrame{
 	
 	class CombinePanel extends JPanel {
 		//컴포넌트 1, 컴포넌트 2, 패널 구성시 좌,우 margin 공간을 없애기 위한 Flag
-		public CombinePanel(Component[] cops, boolean isBorder) {
+		public CombinePanel(Component[] cops, int borderWidth, int borderHeight) {
 			//Margin이 필요하지 않을 때
-			if(!isBorder) {
-				setLayout(new FlowLayout(FlowLayout.LEFT,0,0));	
-			}
-			else {
-				setLayout(new FlowLayout(FlowLayout.LEFT,15,0));	
-			}
+			
+			setLayout(new FlowLayout(FlowLayout.LEFT,borderWidth,borderHeight));
+			
 			for (Component c: cops) {
 				add(c);
 			}
 		}
 	}
 	
+	//사진크기 변환 함수입니다.
+	//https://memorynotfound.com/java-resize-image-fixed-width-height-example/
+    private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resized;
+    }
+	
+    class PictureManageButtonListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource().equals(pictureSearch)) {
+				
+			}
+			else if (e.getSource().equals(add)){
+				
+			}
+			else if (e.getSource().equals(remove)){
+				
+			}
+			else if (e.getSource().equals(register)){
+				
+			}
+			else if (e.getSource().equals(cancel)){
+				dispose();
+			}
+		}
+    	
+    }
+    
+    class PictureManageFileMouseListener extends MouseAdapter{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			super.mouseClicked(e);
+			if(e.getButton()==1) {
+				
+			}
+		}
+    	
+    }
+    
+    class PictureManageSeletedFileMouseListener extends MouseAdapter{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			super.mouseClicked(e);
+			if(e.getButton()==1) {
+				
+			}
+		}
+    	
+    }
+    
 	public static void main(String[] args) throws IOException {
 		new PictureManage();
 	}
