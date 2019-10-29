1790eed0916e4e527c849314dbe43ee6babdb666
JerryKwon <kwonyoung234@gmail.com>
2019-10-30 00:34:39 +0900

    인수자등록팝업(비회원 등록, 반환등록, 보호종료 기능 구현 및 테스트 완료)

--- src/asmr/ReqPrsnRegist.java
+++ src/asmr/ReqPrsnRegist.java
@@ -13,13 +13,21 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
+import javax.management.Query;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -34,14 +42,25 @@ public class ReqPrsnRegist extends JFrame {
 	private JRadioButton cust,nonCust;
 	private JComboBox<String> cbCustSearch;
 	
+	private String abanNo;
+	
 	private JTable eCustList;
 	private JScrollPane custListScroll;
 	
-	private final String[] col1 = {"이름","생년월일","주소"};
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String user = "asmr";
+	private String password = "asmr";
+	
+	private Connection con = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	private ResultSetMetaData rsmd = null;
+	
+	private final String[] col1 = {"이름","아이디","주소"};
 	
 	private DefaultTableModel model1 = new DefaultTableModel(col1,0);
 	
-	private final String[] custSearchDiv = {"이름","생년월일"};
+	private final String[] custSearchDiv = {"이름","아이디"};
 	
 	private Color blue = new Color(22,155,213);
 	private Color white = new Color(255,255,255);
@@ -55,7 +74,7 @@ public class ReqPrsnRegist extends JFrame {
 	GridBagLayout gridBagLayout;
 	GridBagConstraints gridBagConstraints;
 	
-	public ReqPrsnRegist() {
+	public ReqPrsnRegist(String abanNo) {
 		
 		gridBagLayout = new GridBagLayout();		
 		gridBagConstraints = new GridBagConstraints();
@@ -64,6 +83,8 @@ public class ReqPrsnRegist extends JFrame {
 		reqPrsnRegistButtonListener = new ReqPrsnRegistButtonListener();
 		custItemListener = new CustItemListener();
 		nonCustItemListener = new NonCustItemListener();
+		
+		this.abanNo = abanNo;
 		
 		vReqPrsnType = new JLabel("인수자구분");
 		
@@ -93,6 +114,10 @@ public class ReqPrsnRegist extends JFrame {
 		eCustList.addMouseListener(reqRegistMouseListener);
 		custListScroll = new JScrollPane(eCustList);
 		custListScroll.setPreferredSize(new Dimension(350,100));
+		eCustList.getColumnModel().getColumn(0).setPreferredWidth(75);
+		eCustList.getColumnModel().getColumn(1).setPreferredWidth(100);
+		eCustList.getColumnModel().getColumn(2).setPreferredWidth(175);
+		
 		
 		vNonCustRegist = new JLabel("비회원등록");
 		vNonCustRegist.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
@@ -102,7 +127,7 @@ public class ReqPrsnRegist extends JFrame {
 		
 		vAddress = new JLabel("주소");
 		xAddress = new JTextField(15);
-		xAddress.setEnabled(false);
+		xAddress.setEditable(false);
 		addressSearch = new JButton("검색");
 		addressSearch.setBackground(blue);
 		addressSearch.setForeground(white);
@@ -130,6 +155,7 @@ public class ReqPrsnRegist extends JFrame {
 		
 		//기본적으로 회원 활성화
 		activateCust();
+		GetCustList();
 		
 		ReqPrsnRegistView();
 		
@@ -248,9 +274,11 @@ public class ReqPrsnRegist extends JFrame {
 		addressSearch.setEnabled(false);
 		xPhoneNum.setEnabled(false);
 		
+		
 		xCustSearch.setEnabled(true);
 		custSearch.setEnabled(true);
 		cbCustSearch.setEnabled(true);
+		eCustList.setEnabled(true);
 	}
 	
 	private void activateNonCust() {
@@ -264,6 +292,8 @@ public class ReqPrsnRegist extends JFrame {
 		xCustSearch.setEnabled(false);
 		custSearch.setEnabled(false);
 		cbCustSearch.setEnabled(false);
+		eCustList.setEnabled(false);
+		eCustList.getSelectionModel().clearSelection();
 		
 	}
 	
@@ -273,13 +303,27 @@ public class ReqPrsnRegist extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			if(e.getSource().equals(custSearch)) {
+				String text = xCustSearch.getText();
 				
+				String custType = (String)cbCustSearch.getSelectedItem();
+				switch(custType) {
+				case "이름":
+					SearchCust(text, true);
+					break;
+				case "아이디":
+					SearchCust(text, false);
+					break;
+				}
 			}
 			else if(e.getSource().equals(addressSearch)) {
-				
+				new AddressSearch(xAddress);
 			}
 			else if(e.getSource().equals(returning)) {
-				
+				int result = JOptionPane.showConfirmDialog(null, "해당 동물을 반환처리 하시겠습니까?", "반환처리", JOptionPane.YES_NO_OPTION);
+				if(result == JOptionPane.OK_OPTION) {
+					ReturnProtAni();
+					dispose();
+				}
 			}
 			else if(e.getSource().equals(cancel)) {
 				dispose();
@@ -301,6 +345,218 @@ public class ReqPrsnRegist extends JFrame {
 		
 	}
 	
+	private void ReturnProtAni() {
+		
+		//일단 회원이던 비회원이던 현재 보호중인 동물의 보호종료일자를 오늘날자로 변경해야함. 하지만 반대로하는 것이 좋을 것 같음.
+		//반대라는 말은 반환에 먼저 등록하고 그 후에 날짜를 업데이트하자는 뜻
+		boolean isCust = cust.isSelected();
+		
+		String name = null;
+		String id = null;
+		String addr = null;
+		String phoneNum = null;
+		
+		if(isCust) {
+			
+			int clickedRow = eCustList.getSelectedRow();
+			name = (String)eCustList.getValueAt(clickedRow, 0);
+			id = (String)eCustList.getValueAt(clickedRow, 1);
+			String abanNo = this.abanNo;
+			
+			//반환등록
+			StringBuffer query1 = new StringBuffer("INSERT INTO RET(RET_NO,RET_DATE,PROT_NO,CUST_NO) ");
+			query1.append("SELECT ");
+			query1.append("	CASE WHEN SUBSTR(RET_NO,1,8) = to_char(TRUNC(SYSDATE),'yyyymmdd') ");
+			query1.append("	THEN to_char(TRUNC(SYSDATE),'yyyymmdd') || CASE WHEN SUBSTR(RET_NO,10,1) = '9' THEN to_char(SUBSTR(RET_NO,9,1)+1) ELSE SUBSTR(RET_NO,9,1) END || CASE WHEN SUBSTR(RET_NO,10,1)='9' THEN '0' ELSE to_char(SUBSTR(RET_NO,10,1)+1) END");
+			query1.append("	ELSE to_char(TRUNC(SYSDATE),'yyyymmdd') || '01' END RET_NO,");
+			query1.append("	TRUNC(SYSDATE) RET_DATE, ");
+			query1.append("	(SELECT PROT_NO FROM PROT WHERE ABAN_NO='"+abanNo+"' AND PROT_END_DATE=to_date('9999-12-31','YYYY-MM-DD')) PROT_NO, ");
+			query1.append("	(SELECT CUST_NO FROM CUST WHERE CUST_NAME='"+name+"' AND ID='"+id+"') CUST_NO ");
+			query1.append("FROM( ");
+			query1.append("	SELECT NVL(RET_NO,0) RET_NO ");
+			query1.append("	FROM( ");
+			query1.append("		SELECT MAX(RET_NO) RET_NO FROM RET) ");
+			query1.append(") ");
+			
+			//보호종료일자변경
+//			System.out.println(abanNo);
+			StringBuffer query2 = new StringBuffer("UPDATE PROT ");
+			query2.append("SET PROT_END_DATE = trunc(sysdate) ");
+			query2.append("WHERE PROT_NO=(SELECT PROT_NO FROM PROT WHERE ABAN_NO='"+abanNo+"' AND PROT_END_DATE=to_date('9999-12-31','YYYY-MM-DD')) ");
+			
+			connection();
+			
+			try {
+				pstmt = con.prepareStatement(query1.toString());
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					con.commit();
+				}
+
+				pstmt = con.prepareStatement(query2.toString());
+				System.out.println(pstmt.executeUpdate());
+				
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			disconnection();
+		}
+		else {
+			name = xName.getText();
+			addr = xAddress.getText();
+			phoneNum = xPhoneNum.getText();
+			String abanNo = this.abanNo;
+			
+			//신규 비회원 등록
+			StringBuffer query1 = new StringBuffer("INSERT INTO CUST(CUST_NO,CUST_NAME,ADDR,TEL_NO,CUST_TP) ");
+			query1.append("SELECT ");
+			query1.append("	(SELECT NVL(CUST_NO,0)+1 CUST_NO ");
+			query1.append("	FROM(SELECT MAX(cust_no) cust_no FROM CUST)) CUST_NO, ");
+			query1.append("	'"+name+"' CUST_NAME, ");
+			query1.append("	'"+addr+"' ADDR, ");
+			query1.append("	'"+phoneNum+"' TEL_NO, ");
+			query1.append("	'n' CUST_TP ");
+			query1.append("FROM DUAL ");
+			
+			//반환등록
+			StringBuffer query2 = new StringBuffer("INSERT INTO RET(RET_NO,RET_DATE,PROT_NO,CUST_NO) ");
+			query2.append("SELECT ");
+			query2.append("	CASE WHEN SUBSTR(RET_NO,1,8) = to_char(TRUNC(SYSDATE),'yyyymmdd') ");
+			query2.append("	THEN to_char(TRUNC(SYSDATE),'yyyymmdd') || CASE WHEN SUBSTR(RET_NO,10,1) = '9' THEN to_char(SUBSTR(RET_NO,9,1)+1) ELSE SUBSTR(RET_NO,9,1) END || CASE WHEN SUBSTR(RET_NO,10,1)='9' THEN '0' ELSE to_char(SUBSTR(RET_NO,10,1)+1) END");
+			query2.append("	ELSE to_char(TRUNC(SYSDATE),'yyyymmdd') || '01' END RET_NO,");
+			query2.append("	TRUNC(SYSDATE) RET_DATE, ");
+			query2.append("	(SELECT PROT_NO FROM PROT WHERE ABAN_NO='"+abanNo+"' AND PROT_END_DATE=to_date('9999-12-31','YYYY-MM-DD' )) PROT_NO, ");
+			query2.append("	(SELECT CUST_NO FROM CUST WHERE CUST_NAME='"+name+"' AND ADDR='"+addr+"' AND TEL_NO='"+phoneNum+"' ) CUST_NO ");
+			query2.append("FROM( ");
+			query2.append("	SELECT NVL(RET_NO,0) RET_NO ");
+			query2.append("	FROM( ");
+			query2.append("		SELECT MAX(RET_NO) RET_NO FROM RET) ");
+			query2.append(") ");
+			
+			//보호종료일자변경
+			StringBuffer query3 = new StringBuffer("UPDATE PROT ");
+			query3.append("SET PROT_END_DATE = trunc(sysdate) ");
+			query3.append("WHERE PROT_NO=(SELECT PROT_NO FROM PROT WHERE ABAN_NO='"+abanNo+"' AND PROT_END_DATE=to_date('9999-12-31','YYYY-MM-DD')) ");
+			
+			
+			connection();
+			
+			try {
+				pstmt = con.prepareStatement(query1.toString());
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					con.commit();
+				}
+		
+				pstmt = con.prepareStatement(query2.toString());
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					con.commit();
+				}
+		
+				pstmt = con.prepareStatement(query3.toString());
+				System.out.println(pstmt.executeUpdate());
+		
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+			disconnection();
+		
+		}
+	}
+	
+	private void GetCustList() {
+		
+		connection();
+		
+		StringBuffer query = new StringBuffer("SELECT /*+INDEX_ASC(CUST CUST_PK) */ CUST_NO,CUST_NAME,ID,ADDR FROM CUST WHERE CUST_TP='m' ");
+		
+		try {
+			pstmt = con.prepareStatement(query.toString());
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				model1.addRow(new Object[] {rs.getString("CUST_NAME"),rs.getString("ID"),rs.getString("ADDR")});
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		disconnection();
+	}
+	
+	private void SearchCust(String text, boolean isName) {
+		model1.setRowCount(0);
+		
+		connection();
+		
+		StringBuffer query = new StringBuffer();
+		
+		if(isName) {
+			query.append("SELECT /*+INDEX_ASC(CUST CUST_PK) */ CUST_NO,CUST_NAME,ID,ADDR FROM CUST ");
+			query.append("WHERE CUST_NAME='"+text+"' ");
+		
+		}
+		else {
+			query.append("SELECT /*+INDEX_ASC(CUST CUST_PK) */ CUST_NO,CUST_NAME,ID,ADDR FROM CUST ");
+			query.append("WHERE ID='"+text+"' ");
+			
+		}
+		
+		try {
+			pstmt = con.prepareStatement(query.toString());
+			rs = pstmt.executeQuery();
+		
+		while(rs.next()) {
+			model1.addRow(new Object[] {rs.getString("CUST_NAME"),rs.getString("ID"),rs.getString("ADDR")});
+		}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		disconnection();
+	}
+	
+	// 데이터베이스 연결
+
+    public void connection() {
+
+         try {
+
+                  Class.forName("oracle.jdbc.driver.OracleDriver");
+
+                  con = DriverManager.getConnection(url,user,password);
+
+
+         } catch (ClassNotFoundException e) {
+        	 e.printStackTrace();
+         } catch (SQLException e) {
+        	 e.printStackTrace();
+         }
+
+    }
+
+    // 데이터베이스 연결 해제
+    public void disconnection() {
+
+        try {
+
+             if(pstmt != null) pstmt.close();
+
+             if(rs != null) rs.close();
+
+             if(con != null) con.close();
+
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+
+    }
+		
+		
+	
    private void ChangeFont(JComponent[] comps, Font font) {
     	for(JComponent comp: comps) {
     		comp.setFont(font);
@@ -308,6 +564,6 @@ public class ReqPrsnRegist extends JFrame {
     }
 
    public static void main(String[] args) {
-		new ReqPrsnRegist();
+		new ReqPrsnRegist(null);
 	}
 }
